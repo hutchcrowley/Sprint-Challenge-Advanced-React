a5ef2c88fe15e44569d4a95c9ab1e80aec10c6df
Hutch Crowley <tothecoda@gmail.com>
2019-12-19 08:42:27 -0600

    Udate- finish class component, display API data

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,60 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import axios from 'axios'
+import PlayerCard from './Components/PlayerCard'
+
+import './App.css'
+
+class App extends React.Component {
+  constructor (props) {
+    super(props)
+    this.state = {
+      players: []
+    }
+  }
+
+  componentDidMount () {
+    console.log(this.state)
+    axios
+      .get(`http://localhost:5000/api/players`)
+      .then(res => {
+        const playerInfo = res.data
+        console.log(res.data)
+        this.setState({
+          players: playerInfo
+        })
+      })
+      .catch(err => console.log('Error: data not returned from server', err))
+  }
+
+  render () {
+    console.log(
+      'Players data after render method is called',
+      this.state.players
+    )
+
+    return (
+      <div className='App'>
+        <div className='header'>
+          <h1>Women's World Cup Players 2019</h1>
+          <h2>WorldWide Ranking by Google Search Interest</h2>
+          <p>For the period of June 2019 - July 2019</p>
+        </div>
+        <div className='body'>
+          {this.state.players.map(player => {
+            return (
+              <PlayerCard
+                key={player.id}
+                id={player.id}
+                playerName={player.name}
+                playerCountry={player.country}
+              />
+            )
+          })}
+        </div>
+      </div>
+    )
+  }
 }
 
-export default App;
+export default App
